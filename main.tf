7b7b273e11c20813ce0ec9ee1f6f026e866f617d
Amar Minhas <Amar.minhas@gmail.com>
2023-02-27 22:29:16 -0800

    initializing with simple config

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+module "nginx" {
+  source = "./nginx"
+  name = "nginx"
+}
